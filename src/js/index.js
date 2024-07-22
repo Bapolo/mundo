6361c51a33cbea4ec7810266123af21b2b29203c
Jesus Pedro Bapolo <145621406+Bapolo@users.noreply.github.com>
2024-07-22 23:48:42 +0100

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1017,7 +1017,7 @@ window.addEventListener("load", () =>
                         }
                     })
                     
-                    pontos.innerHTML = `Conheça o mundo: <p><br><br>Pontuação <br> ${pontuacao.value}</p>`
+                    pontos.innerHTML = `Conheça o mundo: <p><br>Pontuação <br> ${pontuacao.value}</p>`
                     botaoClicado.style.backgroundColor = "#f00"
                     setTimeout(() => voltarNaTelaInicial(), 1000)                    
                 }
